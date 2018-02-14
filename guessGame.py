ea102ba6167d5fec33d3cf096a7309da7d8c6d08
G00364694 <36238561+G00364694@users.noreply.github.com>
2018-02-14 00:33:31 +0000

    Update guessGame.py

--- guessGame.py
+++ guessGame.py
@@ -6,7 +6,7 @@ from random import randint
 
 target = randint(1, 100)
 guess  = 0
-print("Target is: ", target)
+#print("Target is: ", target)
 print ("Guess a number between 1 and 100:  ")
 while guess != target:
     guess = int(input("Please enter your guess:   "))
