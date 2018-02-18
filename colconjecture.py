65428e492167ad90915054b2a65c383fac8d71a8
Francis Adepoju <G00364694@gmit.ie>
2018-02-18 18:03:08 +0000

    New changes made

--- colconjecture.py
+++ colconjecture.py
@@ -1,18 +1,18 @@
-# Francis Adeepoju. 7 Feb 2018
+# Francis Adepoju. 7 Feb 2018
 # The Collatz conjecture program
 # https://en.wikipedia.org/wiki/Collatz_conjecture
-# see moification in git
+# see moidication in git
 
 intVar = int(input("Please Enter an Integer number: "))   # Take integer input
-#intVar = 12
-#if (intVar != ord('\t') ): #if null value ids not supplied
+# intVar = 12
+# If (intVar != ord('\t') ): #if null value ids not supplied
 while intVar != 1: 
-    print("%3d" %intVar)
+    print("% 3d" % intVar)
     if (intVar % 2 == 0):
         intVar = intVar/2               #divide intVar by 2
     else:
         intVar = (intVar * 3) + 1       #multiply by 3 and add 1 to intVar
 
-print ("%3d" %intVar)                   # End of while loop
+print ("% 3d" % intVar)                   # End of while loop
 
 
---
+++ sumOf35.py
@@ -0,0 +1,17 @@
+#Francis Adepoju, 2018-02-14
+# Sum of numbers multiples of 3 or 5... Euler problem 1
+# https://projecteuler.net/problem=1
+
+summ = 0
+cnt = 1
+
+while cnt < 1000:
+    if cnt % 3 == 0 or cnt % 5 == 0:
+        summ += cnt
+    cnt += 1
+print("Sum of the 3 or 5 multiples, less than 1000 is: ", summ)
+
+
+
+
+
