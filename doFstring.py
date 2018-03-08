f7a0eb164713352773fdc98fac0100daf6261c27
Francis Adepoju <G00364694@gmit.ie>
2018-03-08 22:40:04 +0000

    added factorial script

---
+++ doFstring.py
@@ -0,0 +1,6 @@
+# use python F-String
+
+for i in range(1,11):
+    print(f'{i}\t{i**2}\t{i**3}\t{i**4}\t{i**5}\t{i**6}')
+
+
---
+++ factorial.py
@@ -0,0 +1,21 @@
+# # Francis Adepoju. 8 March 2018
+# https://stackoverflow.com/questions/35467303/write-factorial-with-while-loop-python
+# Write a Python script containing a function called factorial() that 
+# takes a single input/argument which is a positive integer and returns 
+# its factorial. The factorial of a number is that number multiplied by 
+# all of the positive numbers less than it. For example, 
+# the factorial of 5 is 5x4x3x2x1 which equals 120. 
+# You should, in your script, test the function by calling it with the 
+# values 5, 7, and 10.
+
+
+def factorial(inNum):
+    num = 1
+    while inNum >= 1:
+        num *= inNum        # num = num * inNum
+        inNum = inNum - 1
+    return num      
+
+print(f'The factorial of 5 is:\t {factorial(5)}')  
+print(f'The factorial of 7 is:\t {factorial(7)}')
+print(f'The factorial of 10 is:\t {factorial(10)}')
