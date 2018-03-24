c8356ece21cb9b87a328fbb38107692ef690198c
Francis Adepoju <G00364694@gmit.ie>
2018-03-24 12:32:24 +0000

    deleted commment6

--- doFstring.py
+++ doFstring.py
@@ -3,5 +3,4 @@
 for i in range(1,11):
     print(f'{i}\t{i**2}\t{i**3}\t{i**4}\t{i**5}\t{i**6}')
 # printing using the old way... Testing Comment on    
-# printing using the old way... Testing Comment on    
 
