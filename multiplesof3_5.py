f42ef562e752250964cb2e2dfc9d1c6b0fb17bb3
Francis Adepoju <francis.adepoju@gmail.com>
2018-03-24 12:19:38 +0000

    Added Comments on line 9

--- multiplesof3_5.py
+++ multiplesof3_5.py
@@ -4,9 +4,10 @@
 summ = 0
 i = 1
 
+
 while i < 1000:
+    # For all integers either divisible by 3 or by 5, sum them up into variable called summ
     if i % 3 == 0 or i % 5 == 0:
         summ += i
     i += 1
 print("Sum of divisible of 3 or 5 les than 1000 equals:  ", summ)
-
