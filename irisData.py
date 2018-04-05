bdd5d23e63b91e3196f8c8b1b04b2f2b607002e7
G00364694 <36238561+G00364694@users.noreply.github.com>
2018-04-05 10:20:11 +0100

    Update irisData.py

--- irisData.py
+++ irisData.py
@@ -2,12 +2,12 @@
 # The Iris_flower_data_set program
 # http://archive.ics.uci.edu/ml/machine-learning-databases/iris/
 """
-#
-# A Python script that reads in the Iris data set stored in the data subdirectory under
-# the current project and then prints the four numerical values on 
-# each row in a nice format on the screen. The printed data are for:
-# petal length, petal width, sepal length and sepal width. 
-# These values should have the decimal places aligned, with a space between the columns.
+
+ A Python script that reads in the Iris data set stored in the data subdirectory under
+ the current project and then prints the four numerical values on 
+ each row in a nice format on the screen. The printed data are for:
+ petal length, petal width, sepal length and sepal width. 
+ These values should have the decimal places aligned, with a space between the columns.
 """ 
 # Open the input iris data file using the with keyword
 with open("data/iris.csv") as inputFile: 
@@ -21,10 +21,11 @@ with open("data/iris.csv") as inputFile:
         col5 = myData.split(',')[4]  # Not used in this program...
 
         # Data are formatted and printed on the screen as required.
-        print('{0:.1f} {1:.1f} {2:.1f} {3:.1f}'.format(col1, col2, col3, col4))
-        # Using the python f-Strings to print same
+        # print('{0:.1f} {1:.1f} {2:.1f} {3:.1f}'.format(col1, col2, col3, col4))
+        
+        # Using the python3 f-String utility to print the record
         print(f'{col1} {col2} {col3} {col4}')
-        print("--")
+        # print("--")
         #  
 print("End of file processing")                 # End of processing.
 
