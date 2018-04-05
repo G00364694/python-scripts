1f11e1f0aeaa2a3c69a4c6380bfeb1bd2679c73f
G00364694 <36238561+G00364694@users.noreply.github.com>
2018-04-05 10:30:08 +0100

    Update colconjecture.py

--- colconjecture.py
+++ colconjecture.py
@@ -2,12 +2,12 @@
 # The Collatz conjecture program
 # https://en.wikipedia.org/wiki/Collatz_conjecture
 """
-# Python script that starts with an integer and repeatedly applies the Collatz function 
-# (divide by 2 if even, multiply by three and 1 if odd) using a while loop and if statement. 
-# At each iteration, the current value of the integer should be printed to the screen. 
-# You can specify in your code the starting value of 17. 
-# If you wish to enhance your program, have the program ask the user for the integer instead of 
-# specifying a value at the start of your code. 
+ Python script that starts with an integer and repeatedly applies the Collatz function 
+ (divide by 2 if even, multiply by three and 1 if odd) using a while loop and if statement. 
+ At each iteration, the current value of the integer should be printed to the screen. 
+ You can specify in your code the starting value of 17. 
+ If you wish to enhance your program, have the program ask the user for the integer instead of 
+ specifying a value at the start of your code. 
 """
 # Accept an integer input from the Keyboard
 print("*******************************")
@@ -24,6 +24,7 @@ while intVar != 1:
         intVar = (intVar * 3) + 1           
 # Print final result to screen
 print ("% 3d" % intVar)                     # End of while loop
-print(f'{intVar}')                          # Use the f-string feature of python-3
+# Use the f-string feature of python-3
+print(f'{intVar}')                          
 
 
