46d81e77de3705bf36fb323c043bfab686f2c021
G00364694 <36238561+G00364694@users.noreply.github.com>
2018-04-05 10:22:13 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,13 +1,13 @@
 # # Francis Adepoju. 8 March 2018   Ex. #6
 # https://stackoverflow.com/questions/35467303/write-factorial-with-while-loop-python
 """
-# Write a Python script containing a function called factorial() that 
-# takes a single input/argument which is a positive integer and returns 
-# its factorial. The factorial of a number is that number multiplied by 
-# all of the positive numbers less than it. For example, 
-# the factorial of 5 is 5x4x3x2x1 which equals 120. 
-# You should, in your script, test the function by calling it with the 
-# values 5, 7, and 10.
+ Write a Python script containing a function called factorial() that 
+ takes a single input/argument which is a positive integer and returns 
+ its factorial. The factorial of a number is that number multiplied by 
+ all of the positive numbers less than it. For example, 
+ the factorial of 5 is 5x4x3x2x1 which equals 120. 
+ You should, in your script, test the function by calling it with the 
+ values 5, 7, and 10.
 """
 
 def factorial(inNum):
@@ -18,6 +18,7 @@ def factorial(inNum):
     # return final result to the caller
     return num              
 
+# Test the function with 5, 7 and 10
 print(f'The factorial of 5 is:\t {factorial(5)}')  
 print(f'The factorial of 7 is:\t {factorial(7)}')
 print(f'The factorial of 10 is:\t {factorial(10)}')
