8006e9870c93d0661e76d8238049ad24203b47d7
G00364694 <36238561+G00364694@users.noreply.github.com>
2018-04-05 10:28:14 +0100

    Update leastDiv.py

--- leastDiv.py
+++ leastDiv.py
@@ -1,22 +1,22 @@
 # Francis Adepoju, 2018-02-14		Ex. #4
-"""# the smallest positive number that is evenly divisible by all of the numbers from 1 to 20... Euler problem 5
-# 
-# ... https://www.w3resource.com/euler-project/euler-problem5.php#
-# Given that 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-# 
-# The number in question must be divisible by all of the numbers between 1 and 20 (assume inclusive for the ranges).
-# As a result, it must be divisible by all of the numbers between 1 and 10. The smallest number that is divisible
-# by all the numbers between 1 and 10 (given by Project Euler) is 2520. Thus, the number in question here must be a
-# multiple of 2520.
+""" The smallest positive number that is evenly divisible by all of the numbers from 1 to 20... Euler problem 5
+ 
+ ... https://www.w3resource.com/euler-project/euler-problem5.php#
+ Given that 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ 
+ The number in question must be divisible by all of the numbers between 1 and 20 (assume inclusive for the ranges).
+ As a result, it must be divisible by all of the numbers between 1 and 10. The smallest number that is divisible
+ by all the numbers between 1 and 10 (given by Project Euler) is 2520. Thus, the number in question here must be a
+ multiple of 2520.
 
-# The program to find this number will start at 2, iterate to 20, and for each of those numbers it will divide by every 
-# known prime less than that number (technically any prime less than the square root of the number) to determine their factorization. 
-# We will find the numbers that are prime as we go (they won't be divisible by any lesser prime, by definition).
+ The program to find this number will start at 2, iterate to 20, and for each of those numbers it will divide by every 
+ known prime less than that number (technically any prime less than the square root of the number) to determine their factorization. 
+ We will find the numbers that are prime as we go (they won't be divisible by any lesser prime, by definition).
 """
-def gcd(x, y):
-	#return y and gcd(y, x % y) or x     # Compute the greatest common denominator (gcd) function
-  	# return (y and gcd(y, x % y) or x)	 # This line imlementation works
+# Define the function to Compute the greatest common denominator (gcd)
+def gcd(x, y):    
+  	# return (y and gcd(y, x % y) or x)	 # This inline imlementation also works
 	while x != 0 and y != 0:
 		if x > y:
 			x = x % y
@@ -27,7 +27,7 @@ def gcd(x, y):
 		return y
 	else: 
 		return x		
-# Function to compute the lcm of two numbers
+# Define the Function to compute the lcm of two numbers
 def lcm(x, y):
 	return (x * y / gcd(x, y))
 
@@ -38,5 +38,6 @@ for i in range(1, 21):
 	print("factor found :", int(smallestInteger)) 
 print("Euclidean Algorithm Implementation yields: ", int(smallestInteger))
 print("")
+# More test
 print (f'TEST: The gcd of 221 and 323 is {gcd(221, 323)}')
 
